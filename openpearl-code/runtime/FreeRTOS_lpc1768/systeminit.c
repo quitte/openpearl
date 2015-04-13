7346867d9f836e0abb527f2e875b6b5e7cf5f8c8
Jonas Meyer <quitte@gmail.com>
2015-04-13 02:18:55 +0200

    change systeminit to use less of the lpclib 100Mhz instead of 96.

--- openpearl-code/runtime/FreeRTOS_lpc1768/systeminit.c
+++ openpearl-code/runtime/FreeRTOS_lpc1768/systeminit.c
@@ -1,46 +1,69 @@
 #include "chip.h"
-#include "clock_17xx_40xx.h"
-
 /* System oscillator rate and RTC oscillator rate */
 const uint32_t OscRateIn = 12000000;
 const uint32_t RTCOscRateIn = 32768;
 
-static void clockInit() {
-	/* Disconnect the Main PLL if it is connected already */
-	if (Chip_Clock_IsMainPLLConnected()) {
-		Chip_Clock_DisablePLL(SYSCTL_MAIN_PLL, SYSCTL_PLL_CONNECT);
-	}
+static void pllfeed(int pllnum) {
+	LPC_SYSCON->PLL[pllnum].PLLFEED = 0xAA;
+	LPC_SYSCON->PLL[pllnum].PLLFEED = 0x55;
+}
 
-	/* Disable the PLL if it is enabled */
-	if (Chip_Clock_IsMainPLLEnabled()) {
-		Chip_Clock_DisablePLL(SYSCTL_MAIN_PLL, SYSCTL_PLL_ENABLE);
-	}
+static void ClockInit() {
+	/* Sets the CPU clock to 100Mhz from the external 12Mhz
+	 * crystal, following the instructions of the NXP UM10360
+	 * Manual, 4.5.13 */
 
-	/* Enable the crystal */
-	if (!Chip_Clock_IsCrystalEnabled())
-		Chip_Clock_EnableCrystal();
-	while(!Chip_Clock_IsCrystalEnabled()) {}
+	/* Disconnect the Main PLL if it is connected already
+	 * Which is the same as enabling, disconnected PLL for
+	 * all intents and purposes*/
+	LPC_SYSCON->PLL[0].PLLCON = (1<<1);pllfeed(0);
+	/* Disable the PLL*/
+	LPC_SYSCON->PLL[0].PLLCON = 0;pllfeed(0);
+	/* CPU Clock divider = 1 to improve speed */
+	LPC_SYSCON->CCLKSEL = 0;
+	/* Enable the crystal, wait for lock*/
+	LPC_SYSCON->SCS=(1<<5);
+	while(!(LPC_SYSCON->SCS&(1<<6)));
+	/* Pick the external Oscillator */
+	LPC_SYSCON->CLKSRCSEL = (1<<0);
+	/* FCCO = ((24+1) * 2 * (12MHz/2)) = 300MHz */
+	LPC_SYSCON->PLL[0].PLLCFG = (1<<16)|(24<<0);pllfeed(0);
+	/* Enable the PLL */
+	LPC_SYSCON->PLL[0].PLLCON = (1<<0);pllfeed(0);
+	/*Wait for PLL lock*/
+	while (!(LPC_SYSCON->PLL[0].PLLSTAT&(1<<26))) {} /* Wait for the PLL to Lock */
+	/* CPUClock Divider: 300Mhz/3=100Mhz */
+	LPC_SYSCON->CCLKSEL = 2;
+	/*Connect the PLL*/
+	LPC_SYSCON->PLL[0].PLLCON = (1<<0)|(1<<1);pllfeed(0);
 
-	/* Set PLL0 Source to Crystal Oscillator */
-	Chip_Clock_SetCPUClockDiv(0);
-	Chip_Clock_SetMainPLLSource(SYSCTL_PLLCLKSRC_MAINOSC);
+	/* Put all the peripheral clocks to their CCLK/4 reset value */
+	LPC_SYSCON->PCLKSEL[0] = 0;
+	LPC_SYSCON->PCLKSEL[1] = 0;
 
-	/* FCCO = ((15+1) * 2 * 12MHz) / (0+1) = 384MHz */
-	Chip_Clock_SetupPLL(SYSCTL_MAIN_PLL, 15, 0);
+	/* Make timer0 run at CCLK */
+	LPC_SYSCON->PCLKSEL[0] = (1<<2);
+}
 
-	Chip_Clock_EnablePLL(SYSCTL_MAIN_PLL, SYSCTL_PLL_ENABLE);
+static void USBClockInit(){
+	/* Disconnect and disable PLL*/
+	LPC_SYSCON->PLL[1].PLLCON = 0;pllfeed(1);
+	/* USBClock M=4, P=2  */
+	LPC_SYSCON->PLL[1].PLLCFG = (2<<5)|(4<<0);pllfeed(1);
+	/* Enable and connect the PLL */
+	LPC_SYSCON->PLL[1].PLLCON = (1<<0)|(1<<1);pllfeed(1);
+	/*Wait for PLL lock*/
+	while (!(LPC_SYSCON->PLL[1].PLLSTAT&(1<<10))) {} /* Wait for the PLL to Lock */
+}
 
-	/* 384MHz / (3+1) = 96MHz */
-	Chip_Clock_SetCPUClockDiv(3);
-	while (!Chip_Clock_IsMainPLLLocked()) {} /* Wait for the PLL to Lock */
-
-	Chip_Clock_EnablePLL(SYSCTL_MAIN_PLL, SYSCTL_PLL_CONNECT);
-
-
+static void PowerInit(){
+	/* Set Peripheral Power to the Reset value */
+	LPC_SYSCON->PCONP = 0x3BE;
 }
 
 void SystemInit() {
-	clockInit();
+	ClockInit();
+//	PowerInit();
 	SystemCoreClockUpdate();
 }
 
